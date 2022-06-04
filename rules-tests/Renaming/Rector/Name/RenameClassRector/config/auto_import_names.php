296367edc8d7459b1b2c00e7f2109c3fa88216df
GitHub Action <action@github.com>
2022-06-04 12:01:30 +0000

    [ci-review] Rector Rectify

--- rules-tests/Renaming/Rector/Name/RenameClassRector/config/auto_import_names.php
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/config/auto_import_names.php
@@ -15,6 +15,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         OldClass::class => NewClass::class,
         SomeServiceClassFirstNamespace::class => SomeServiceClass::class,
-        \Queue::class => \Illuminate\Support\Facades\Queue::class,
+        Queue::class => \Illuminate\Support\Facades\Queue::class,
     ]);
 };
